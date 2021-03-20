8b72f3397c673e25403965a471f5a71a1d2309cb
Dirk Jönsson <dirk.joensson@brainbell.de>
2021-03-20 16:54:08 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -10,14 +10,7 @@ function App() {
         <p>
           Hi there!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I was changed on a feature branch</p>
       </header>
     </div>
   );
